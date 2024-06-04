4c46cd7a407b3206528bb7d8c8f70999e97a4559
Kate Chang <kate56ncl@gmail.com>
2024-06-04 13:21:32 +1000

    Store 6 products as a JS object and render products dynamically to the DOM

---
+++ app.js
@@ -0,0 +1,18 @@
+import products from './products.js';
+
+const productsContainer = document.querySelector('.products');
+
+/*store a minimum of five products in your shop items and 
+render the shop items dynamically to the DOM */
+products.forEach((product, i) => {
+  const container = document.createElement('div');
+  container.classList = `product product-${i + 1}`;
+  container.innerHTML = `
+    ${i === 0 || i == 1 ? '<span class="new-tag">New</span>' : ''}
+    <img src=${product.image} alt="sneaker" />
+    <h4>${product.title}</h4>
+    <p class="price">A$${product.price}</p>
+    <button>Add to Cart</button>
+  `;
+  productsContainer.appendChild(container);
+});
---
+++ products.js
@@ -0,0 +1,11 @@
+// Store each product in your online shop as a JS object (key:value)
+const products = [
+  { title: 'Jordan Spizike Low', price: 220, image: './resources/images/sneaker01.jpg' },
+  { title: 'New Balance Kawhi 4', price: 190, image: './resources/images/sneaker02.jpg' },
+  { title: 'Nike Zoom Vomero 5', price: 168, image: './resources/images/sneaker03.jpg' },
+  { title: 'Adidas Harden 8', price: 230, image: './resources/images/sneaker04.jpg' },
+  { title: 'Nike Tuned 3', price: 280, image: './resources/images/sneaker05.jpg' },
+  { title: 'Adidas Ozmillen Utility', price: 300, image: './resources/images/sneaker06.jpg' },
+];
+
+export default products;
